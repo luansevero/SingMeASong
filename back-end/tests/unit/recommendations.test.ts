91c289d30b1520993bee8d229c33ec83d5884248
Luan Severo <luansevero@icloud.com>
2022-09-25 14:54:56 -0300

    [Feat] UnitTest : POST/recommendatios/downvote

--- back-end/tests/unit/recommendations.test.ts
+++ back-end/tests/unit/recommendations.test.ts
@@ -1,7 +1,6 @@
 import recommendationFactory from "../factories/recommendationFactory";
 import { recommendationRepository } from "../../src/repositories/recommendationRepository";
 import { recommendationService } from "../../src/services/recommendationsService";
-import { not } from "joi";
 
 jest.mock("../../src/repositories/recommendationRepository");
 
@@ -10,16 +9,28 @@ beforeEach(async () => {
     jest.resetAllMocks();
 });
 
+function spyAndMock(
+    functionName :   
+        | "create" 
+        | "findAll" 
+        | "find"
+        | "findByName"
+        | "updateScore"
+        | "getAmountByScore"
+        | "remove"
+    ,
+    value? : any
+){
+    return jest
+        .spyOn(recommendationRepository, functionName)
+        .mockResolvedValue(value)
+}
+
 describe("POST /recommendations", () => {
     it("Insert a new recommendation", async () => {
         const recommendation = recommendationFactory.__createRecommendation("rightLink");
-        const findByName = jest
-            .spyOn(recommendationRepository, "findByName")
-            .mockResolvedValue(null);
-        const create = jest
-            .spyOn(recommendationRepository, "create")
-            .mockResolvedValue();
-
+        const findByName = spyAndMock("findByName", null);
+        const create = spyAndMock("create");
         const { insert } = recommendationService;
 
         await expect(insert(recommendation)).resolves.not.toThrow();
@@ -28,12 +39,8 @@ describe("POST /recommendations", () => {
     });
     it("Inserting a existing recommendation", async () => {
         const recommendation = recommendationFactory.__createRecommendationData();
-        const findByName = jest
-            .spyOn(recommendationRepository, "findByName")
-            .mockResolvedValue(recommendation);
-        const create = jest
-            .spyOn(recommendationRepository, "create")
-            .mockResolvedValue();
+        const findByName = spyAndMock("findByName", recommendation);
+        const create = spyAndMock("create");
 
         const { insert } = recommendationService;
 
@@ -46,12 +53,8 @@ describe("POST /recommendations", () => {
 describe("POST /recommendations/:id/upvote", () => {
     it("Giving a valid ID", async () => {
         const recommendation = recommendationFactory.__createRecommendationData();
-        const find = jest
-            .spyOn(recommendationRepository, "find")
-            .mockResolvedValue(recommendation)
-        const updateScore = jest
-            .spyOn(recommendationRepository, "updateScore")
-            .mockResolvedValue(recommendation)
+        const find = spyAndMock("find", recommendation);
+        const updateScore = spyAndMock("updateScore", recommendation);
 
         const { upvote } = recommendationService;
 
@@ -61,12 +64,8 @@ describe("POST /recommendations/:id/upvote", () => {
     });
     it("Giving a invalid ID", async () => {
         const recommendation = recommendationFactory.__createRecommendationData();
-        const find = jest
-            .spyOn(recommendationRepository, "find")
-            .mockResolvedValue(null)
-        const updateScore = jest
-            .spyOn(recommendationRepository, "updateScore")
-            .mockResolvedValue(recommendation)
+        const find = spyAndMock("find", null);
+        const updateScore = spyAndMock("updateScore", recommendation);
 
         const { upvote } = recommendationService;
 
@@ -74,5 +73,50 @@ describe("POST /recommendations/:id/upvote", () => {
         expect(find).toBeCalled();
         expect(updateScore).not.toBeCalled();
     });
+});
 
+describe("POST /recommendations/:id/downvote", () => {
+    it("Givin a valid ID", async () => {
+        const recommendation = recommendationFactory.__createRecommendationData();
+        const find = spyAndMock("find", recommendation);
+        const updateScore = spyAndMock("updateScore", recommendation);
+        const remove = spyAndMock("remove");
+
+        const { downvote } = recommendationService;
+
+        await expect(downvote(recommendation["id"])).resolves.not.toThrow();
+        expect(find).toBeCalled();
+        expect(updateScore).toBeCalled();
+        expect(remove).not.toBeCalled();
+
+    })
+    it("Givin a valid ID && Deleting the recommendation", async () => {
+        const recommendation = recommendationFactory.__createRecommendationData();
+        const find = spyAndMock("find", recommendation);
+        const updateScore = spyAndMock("updateScore", {
+            ...recommendation,
+            score : -6
+        });
+        const remove = spyAndMock("remove");
+
+        const { downvote } = recommendationService;
+
+        await expect(downvote(recommendation["id"])).resolves.not.toThrow();
+        expect(find).toBeCalled();
+        expect(updateScore).toBeCalled();
+        expect(remove).toBeCalled();
+    })
+    it("Givin a invalid ID", async () => {
+        const recommendation = recommendationFactory.__createRecommendationData();
+        const find = spyAndMock("find", null);
+        const updateScore = spyAndMock("updateScore", recommendation);
+        const remove = spyAndMock("remove")
+
+        const { downvote } = recommendationService;
+
+        await expect(downvote(recommendation["id"])).rejects.toEqual({type: "not_found", message: ""});
+        expect(find).toBeCalled();
+        expect(updateScore).not.toBeCalled();
+        expect(remove).not.toBeCalled();
+    })
 });
